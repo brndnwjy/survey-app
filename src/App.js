96955bab4fc3212c39c4865992ae3c92775cd97b
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-14 11:29:10 +0700

    Feature : navigate between cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -44,49 +45,55 @@ function App() {
     },
   ];
 
+  const [section, setSection] = useState(0);
+
   return (
     <main className="main">
-      <section className="home card">
-        <div>
-          <h1>Home</h1>
-          <h3>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </h3>
-        </div>
-
-        <button className="btn btn-alt">Start Survey</button>
-      </section>
-
-      {/* <section className="card">
-        <div className="header">
-          <small>Question 1/{questions.length}</small>
-          <span>01:00</span>
-        </div>
-
-        <div className="question">
-          <h2>{questions[0].question}</h2>
-          <div className="options">
-            {questions[0].options.map((item, index) => (
-              <>
-                <input
-                  key={index}
-                  id={`option${index + 1}`}
-                  type="radio"
-                  hidden
-                />
-                <label htmlFor={`option${index + 1}`}>{item}</label>
-              </>
-            ))}
+      {section === 0 ? (
+        <section className="home card">
+          <div>
+            <h1>Home</h1>
+            <h3>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </h3>
           </div>
-        </div>
 
-        <button type="button" className="btn">
-          Next
-        </button>
-      </section> */}
+          <button className="btn btn-alt" onClick={() => setSection(1)}>
+            Start Survey
+          </button>
+        </section>
+      ) : (
+        <section className="card">
+          <div className="header">
+            <small>Question 1/{questions.length}</small>
+            <span>01:00</span>
+          </div>
+
+          <div className="question">
+            <h2>{questions[0].question}</h2>
+            <div className="options">
+              {questions[0].options.map((item, index) => (
+                <>
+                  <input
+                    key={index}
+                    id={`option${index + 1}`}
+                    type="radio"
+                    hidden
+                  />
+                  <label htmlFor={`option${index + 1}`}>{item}</label>
+                </>
+              ))}
+            </div>
+          </div>
+
+          <button type="button" className="btn" onClick={() => setSection(0)}>
+            Next
+          </button>
+        </section>
+      )}
     </main>
   );
 }
